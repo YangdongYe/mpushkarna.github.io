aac934e12922d1517d2c3a32c34ef6f8782dba28
mpushkarna <mahimapushkarna@gmail.com>
2016-02-11 18:39:53 -0500

    Visibility 2
    Cleaning Up

--- modules/Timeseries.js
+++ modules/Timeseries.js
@@ -1,117 +1,120 @@
-/*Created By Mahima on 2/4/16 */
 d3.timeSeries = function(){
-
-/*Internal Variables (defaults)*/
 	var w = 800,
 		h = 600,
 		m = {t:25, r:50, b:25, l:50}, 
 		chartW = w - m.l - m.r, 
 		chartH = h - m.t - m.b,
 		layout = d3.layout.histogram(),
-		maxT=80;
+		maxT=100;
 
-	var	timeRange = [new Date(),new Date()], //time range
-		binSize=d3.time.day, //number of bins --> .day specifies every day / stick to week for simplicity
-	valueAccessor = function(d){return d;};//used by d3.layout.histogram
+	var	timeRange = [new Date(),new Date()],
+		binSize=d3.time.day,
+		valueAccessor = function(d){return d;};
 
 	//scales
 	var scaleX = d3.time.scale()
-			.range([0, chartW])
-			.domain(timeRange);
+				.range([0, chartW])
+				.domain(timeRange);
 	var scaleY = d3.scale.linear()
-			.range([chartH,0])
-			.domain([0,maxT]);
+				.range([chartH,0])
+				.domain([0,maxT]);
 
 /*CUSTOM CHART*/
-function exports(selection){ //_selection signifies user input
+function exports(selection){ 
 
 	var layBins = binSize.range(timeRange[0],timeRange[1]);
-		layBins.unshift(timeRange[0]); //adds starting value/timeRange to the start of the array for consideration
-		layBins.push(timeRange[1]);//adds ending value/timeRange to the end of the array for consideration
-
-	// var layout = d3.layout.histogram()
-	layout.range(timeRange)
-		.bins(layBins);
-		//	console.log(layBins); //successfully returns the first and last entry in the array
-		//	console.log(binSize.range(timeRange[0],timeRange[1])); //successfully returns all the selected values in the range
-
+		layBins.unshift(timeRange[0]); 
+		layBins.push(timeRange[1]);
+	layout = d3.layout.histogram();
+	layout
+	.range(timeRange)
+	.bins(layBins);
+		
 /* use a histogram layout  to transform into a series of (x,y)*/
-//set size
+//set size and scale
 	chartW = w - m.l - m.r, 
 	chartH = h - m.t - m.b;
-
-//set scales
 	scaleX.range([0,chartW]).domain(timeRange); 
 	scaleY.range([chartH,0]).domain([0,maxT]); 
 
 selection.each(draw);
-// console.log("Running Man");
 }
 
 
 function draw(d){
-		var _data = layout(d); //d3.layout.histogram(d)
-		// console.log(_data);
+		var _data = layout(d); 
 
-		var line = d3.svg.line()
+		// var line = d3.svg.line()
+		// 			.x(function(d){return scaleX(d.x.getTime() + d.dx/2)})
+		// 			.y(function(d){return scaleY(d.y)})
+		// 			.interpolate('basis');
+		// var area = d3.svg.area()
+		// 			.x(function(d) {return scaleX(d.x.getTime() + d.dx/2)})
+  //  					.y0(chartH)
+  //   				.y1(function(d) {return scaleY(d.y);})
+  //   				.interpolate('basis');
+		// var axisX = d3.svg.axis()
+		// 			.orient('bottom')
+		// 			.scale(scaleX)
+		// 			.ticks(d3.time.year);	
+		var svg = d3.select(this)
+					.selectAll('svg')
+					.data([d]);
+										console.log("I SEE YOU");
+		var svg2 = svg.enter().append('svg');//svgEnter
+ 
+		svg2.append('g')
+			.attr('class','area')
+			.attr('transform','translate('+m.l+','+m.t+')')
+			.append('path');
+
+		svg2.append('g')
+			.attr('class','line')
+			.attr('transform','translate('+m.l+','+m.t+')')
+			.append('path');
+
+		svg2.append('g')
+			.attr('class','axis')
+			.attr('transform','translate('+m.l+','+(m.t+chartH)+')');
+
+		svg.attr('width',w).attr('height',h);
+
+var line = d3.svg.line()
 					.x(function(d){return scaleX(d.x.getTime() + d.dx/2)})
 					.y(function(d){return scaleY(d.y)})
-					.interpolate('basis'); //refers to a linear interpolation
-
-		var area = d3.svg.area()
+					.interpolate('basis');
+svg.select('.line')
+	.select('path')
+	.datum(_data)
+	.attr('d',line);
+	/*line = d3.svg.line()
+					.x(function(d){return scaleX(d.x.getTime() + d.dx/2)})
+					.y(function(d){return scaleY(d.y)})
+					.interpolate('basis');*/
+var area = d3.svg.area()
 					.x(function(d) {return scaleX(d.x.getTime() + d.dx/2)})
    					.y0(chartH)
     				.y1(function(d) {return scaleY(d.y);})
     				.interpolate('basis');
-		var axisX = d3.svg.axis() //axisX
+svg.select('.area')
+	.select('path')
+	.datum(_data)
+	.attr('d',area);
+	/*var area = d3.svg.area()
+					.x(function(d) {return scaleX(d.x.getTime() + d.dx/2)})
+   					.y0(chartH)
+    				.y1(function(d) {return scaleY(d.y);})
+    				.interpolate('basis');*/
+var axisX = d3.svg.axis()
 					.orient('bottom')
 					.scale(scaleX)
 					.ticks(d3.time.year);
-		
-		var svg = d3.select(this)
-					.selectAll('svg')
-					.data([d]);
-
-	console.log("I SEE YOU");
-		
-		var svg2 = svg.enter().append('svg');//svgEnter
-
-
-
-		//moved up //insert shape generators to draw //ref old notes
-// append DOM and draw the (x,y) as a line, and an area
-//same thing, just draw twice, change class and style in CSS
-// var areaAppend = 
-svg2.append('g')
-			.attr('class','area')
-			.attr('transform','translate('+m.l+','+m.t+')')
-			.append('path');
-// var lineAppend = 
-svg2.append('g')
-			.attr('class','line')
-			.attr('transform','translate('+m.l+','+m.t+')')
-			.append('path');
-// var axisAppend = 
-svg2.append('g')
-			.attr('class','axis')
-			.attr('transform','translate('+m.l+','+(m.t+chartH)+')');
-
-svg.attr('width',w).attr('height',h);
-
-
-svg.select('.line')//select all svgs with class area
-	.select('path')//select the appended paths in the class area
-	.datum(_data)//join in many-to-one relationship to _data
-	.attr('d',line); //d is the datum from the this selection joined to the DOM - create d3.svg.area
-
-svg.select('.area')//select all svgs with class area
-	.select('path')//select the appended paths in the class area
-	.datum(_data)//join in many-to-one relationship to _data
-	.attr('d',area); //d is the datum from the this selection joined to the DOM - create d3.svg.area
-
-//append DOM and draw axis - DRAW ONLY X-AXIS NOT Y-
 svg.select('.axis')
-	.call(axisX);//axisX
+	.call(axisX);
+	/*var axisX = d3.svg.axis()
+					.orient('bottom')
+					.scale(scaleX)
+					.ticks(d3.time.year);*/
 }
 
 
@@ -148,8 +151,8 @@ exports.value = function (accessor){
 	return this;
 }
 exports.maxT = function(_y){
-		if(!arguments.length) return maxY;
-		maxY = _y;
+		if(!arguments.length) return maxT;
+		maxT = _y;
 		return this;
 	}
 
--- script.js
+++ script.js
@@ -6,59 +6,46 @@ var timeSeries = d3.timeSeries()
                 .height(h)
                 .timeRange([new Date (2011,6,16), new Date (2013,11,15)])
                 .value(function(d){return d.startTime})
-                .maxT(50)
                 .binSize(d3.time.day);
 
 d3.csv('data/hubway_trips_reduced.csv',parse,dataLoaded);
 
 function dataLoaded(err,rows){
 
-//use this function to sort by origin or destination
-// var sourcePoint = function(rows){return rows.startStation};
+    var sourcePoint = function(rows){return rows.startStation};
 
-//set up for data for small multiples
-//define stations
-var tripsStation= d3.nest()
-                    // .key(sourcePoint)
-                    .key(function(d){return d.startStation})
-                    .entries(rows);
+    var tripsStation= d3.nest()
+                        .key(sourcePoint)
+                        .entries(rows);
 
-//add data to each station
-var plots= d3.select('.container')
-            .selectAll('.plot')
-            .data(tripsStation);
+    var plots= d3.select('.container')
+                    .selectAll('.plot')
+                    .data(tripsStation);
+        plots
+            .enter()
+            .append('div')
+            .attr('class','plot');
 
-console.log (tripsStation); //successfully returns a nested array of trips by sorting order
-
-//oh wait assign a class so it knows where to go and how to look
-plots
-    .enter()
-    .append('div')
-    .attr('class','plot');
-
-//now draw - WHY IS THIS NOT HAPPENING??!
-plots
-    .each(function(d){
-        d3.select(this)
-        .datum(d.values) //get val for nested array of stations in plots
-        .call(timeSeries)
-        .append('h2') //the absence of this shouldnt matter
-        .text(d.key); //the absence of this shouldn't matter
-        })
-
-
+        plots
+            .each(function(d){
+                d3.select(this)
+                .datum(d.values)
+                .call(timeSeries)
+                .append('h2')
+                .text(d.key);
+                })
 }
 
 function parse(d){
     if(+d.duration<0) return;
 
-    return {
-        duration: +d.duration,
-        startTime: parseDate(d.start_date),
-        endTime: parseDate(d.end_date),
-        startStation: d.strt_statn,
-        endStation: d.end_statn
-    }
+        return {
+            duration: +d.duration,
+            startTime: parseDate(d.start_date),
+            endTime: parseDate(d.end_date),
+            startStation: d.strt_statn,
+            endStation: d.end_statn
+        }
 }
 
 function parseDate(date){
